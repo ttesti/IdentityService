aade5d245a71d8f959bd46add07c356b6c0b9436
Thomas Testi <tmtesti@gmail.com>
2026-02-25 16:08:34 -0600

    Updated some extentions and changed some api code

---
+++ Infrastructure/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+
+namespace Infrastructure.Contexts
+{
+    public class ApplicationDbContextFactory
+        : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "..",
+                "WebAPI"
+            );
+
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                config.GetConnectionString("DefaultConnection")
+            );
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
